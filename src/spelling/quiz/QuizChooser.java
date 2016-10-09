891a202b3d4a8326dc56ae7647bab45cc39a4fd7
RemRinRamChi <yyap601@aucklanduni.ac.nz>
2016-10-10 01:12:20 +1300

    Added listeners to all the NZCER lvl buttons in QuizChooser, need to refactor them later

--- src/spelling/quiz/QuizChooser.java
+++ src/spelling/quiz/QuizChooser.java
@@ -61,60 +61,130 @@ public class QuizChooser extends JDialog {
 		getContentPane().add(btn1);
 		
 		JButton btn2 = new JButton("2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(2);
+			}
+		});
 		btn2.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn2.setFocusPainted(false);
 		btn2.setBounds(81, 70, 39, 39);
 		getContentPane().add(btn2);
 		
 		JButton btn3 = new JButton("3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(3);
+			}
+		});
 		btn3.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn3.setFocusPainted(false);
 		btn3.setBounds(130, 70, 39, 39);
 		getContentPane().add(btn3);
 		
 		JButton btn4 = new JButton("4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(4);
+			}
+		});
 		btn4.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn4.setFocusPainted(false);
 		btn4.setBounds(179, 70, 39, 39);
 		getContentPane().add(btn4);
 		
 		JButton btn5 = new JButton("5");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(5);
+			}
+		});
 		btn5.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn5.setFocusPainted(false);
 		btn5.setBounds(228, 70, 39, 39);
 		getContentPane().add(btn5);
 		
 		JButton btn6 = new JButton("6");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(6);
+			}
+		});
 		btn6.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn6.setFocusPainted(false);
 		btn6.setBounds(277, 70, 39, 39);
 		getContentPane().add(btn6);
 		
 		JButton btn7 = new JButton("7");
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(7);
+			}
+		});
 		btn7.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn7.setFocusPainted(false);
 		btn7.setBounds(42, 120, 39, 39);
 		getContentPane().add(btn7);
 		
 		JButton btn8 = new JButton("8");
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(8);
+			}
+		});
 		btn8.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn8.setFocusPainted(false);
 		btn8.setBounds(91, 120, 39, 39);
 		getContentPane().add(btn8);
 		
 		JButton btn9 = new JButton("9");
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(9);
+			}
+		});
 		btn9.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn9.setFocusPainted(false);
 		btn9.setBounds(140, 120, 39, 39);
 		getContentPane().add(btn9);
 		
 		JButton btn10 = new JButton("10");
+		btn10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(10);
+			}
+		});
 		btn10.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn10.setFocusPainted(false);
 		btn10.setBounds(189, 120, 45, 39);
 		getContentPane().add(btn10);
 		
 		JButton btn11 = new JButton("11");
+		btn11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				mainFrame.changeCardPanel("Quiz");
+				mainQuizPanel.startQuiz(11);
+			}
+		});
 		btn11.setFont(new Font("Arial", Font.PLAIN, 9));
 		btn11.setFocusPainted(false);
 		btn11.setBounds(244, 120, 45, 39);
